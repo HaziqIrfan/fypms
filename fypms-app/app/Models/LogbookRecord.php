97400e1138ab4c311be9850193209fbabcc595c8
HaziqIrfan <wanmuhamadhaziqirfan@gmail.com>
2023-04-01 23:53:33 +0800

    setup env

---
+++ fypms-app/app/Models/LogbookRecord.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class LogbookRecord extends Model
+{
+    use HasFactory;
+}
